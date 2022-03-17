035cf2fc9d9ced4e9a8b2179b5da99f10e047421
Birgit Nehrwein <nehrwein@posteo.de>
2022-03-17 10:08:44 +0100

    feat(Navbar): fixed Header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,6 +21,10 @@ const NavBar = () => {
 export default NavBar
 
 const Header = styled.header`
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
   display: flex;
   align-items: center;
   height: 60px;
